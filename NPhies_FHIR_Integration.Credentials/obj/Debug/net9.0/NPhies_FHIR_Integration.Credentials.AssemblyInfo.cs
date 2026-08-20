01d0d7c0617c2ba4d3a8696c3ad8302f03dfbf4e
Sibtain.Alimadad <Sibtain.Alimadad@cloudsolutions.com.sa>
2026-08-20 18:26:00 +0300

    versionchanges

--- NPhies_FHIR_Integration.Credentials/obj/Debug/net9.0/NPhies_FHIR_Integration.Credentials.AssemblyInfo.cs
+++ NPhies_FHIR_Integration.Credentials/obj/Debug/net9.0/NPhies_FHIR_Integration.Credentials.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NPhies_FHIR_Integration.Credentials")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b54e7d396a719cdd7d72285fa5715547bb81b29c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c3a8842b5098cbfbc9a51879c4a428872b092d3")]
 [assembly: System.Reflection.AssemblyProductAttribute("NPhies_FHIR_Integration.Credentials")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NPhies_FHIR_Integration.Credentials")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
